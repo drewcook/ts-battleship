b359fbff54f3b3aa5f116514f6ff679d8abb1961
Drew Cook <drewcook29@gmail.com>
2021-11-20 12:46:53 -0700

    chore: Client app cleanup

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -13,3 +13,15 @@ export const get = async (route: string) => {
 		return status
 	}
 }
+
+export const post = async (route: string) => {
+	const promise = await axios.post(baseUrl + route)
+	const status = promise.status
+	if (status === 200) {
+		const data = promise.data
+		return data
+	} else {
+		console.info('Not OK', status)
+		return status
+	}
+}
